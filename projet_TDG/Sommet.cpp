99abe4d3dbdc798b761a0d3062f67c4d6d865f6d
Moefiz <36449069+Moefiz@users.noreply.github.com>
2018-04-03 14:08:14 +0200

    Classes + lecture matrice

---
+++ projet_TDG/Sommet.cpp
@@ -0,0 +1,16 @@
+#include "Sommet.h"
+
+Sommet::Sommet(): m_name("")
+{
+
+}
+
+Sommet::Sommet(std::string name): m_name(name)
+{
+
+}
+
+Sommet::~Sommet()
+{
+    //dtor
+}
--- projet_TDG/main.cpp
+++ projet_TDG/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
-using namespace std;
+#include "fonctiongraph.h"
+#include "Graphe.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    std::string name;
+
+    std::cout << "Veillez rentrez le nom du fichier : ";
+    std::cin >> name;
+
+    Graphe g1(name);
+
+
+
+    if((&g1)->recuperationInformation()){
+        (&g1)->afficherMatriceAdjacente();
+        (&g1)->creationAretes();
+        (&g1)->affichageRelation();
+    }
+
     return 0;
 }
