4446524be3b98802de7754bf5c21e56a09a7d4a9
Moefiz <36449069+Moefiz@users.noreply.github.com>
2018-04-03 15:26:03 +0200

    fercoq+gui

--- projet_TDG/main.cpp
+++ projet_TDG/main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 
-    std::string name;
+    /*std::string name;
 
     std::cout << "Veillez rentrez le nom du fichier : ";
     std::cin >> name;
@@ -19,7 +19,33 @@ int main()
         (&g1)->afficherMatriceAdjacente();
         (&g1)->creationAretes();
         (&g1)->affichageRelation();
+    }*/
+
+     /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("images");
+
+    /// Un exemple de graphe
+    Graph g;
+    g.make_example();
+
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update();
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
     }
+
+    grman::fermer_allegro();
+
+
 
     return 0;
 }
